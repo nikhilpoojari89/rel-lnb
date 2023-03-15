253c547e0634e7ac44f3a15a3199a4d38c459c9a
Nikhil Poojari <nikhilpoojari89@gmail.com>
2023-03-16 04:11:48 +0530

    Initial Commit

---
+++ index.php
@@ -0,0 +1,128 @@
+<?php $title = "Reliance Nippon Life Nishchit Bhavishya"; ?>
+<?php include 'header.php' ?>
+
+<!--********************** Main Heading Banner Section ***********************-->
+<section class="custom-top-padding mb-0 pb-5 pb-lg-0 top-banner-bg">
+    <div class="container-fluid px-0">
+        <div class="row justify-content-between mx-3">
+            <div class="col-lg-7 col-xl-5 left-section">
+                <div class="form-card py-5">
+                    <h1 class="text-primary main-title vag-r-bold font-weight-bold mb-3 letter-spacing-1 font-60">Reliance Nippon Life<br class="d-none d-lg-block"/> Nishchit Bhavishya</h1>
+                    <span class="text-primary main-sub-title vag-r-bold font-16 letter-spacing-05 font-smooth d-inline-block py-1">A Non-Linked, Non-Participating, Individual Savings Life Insurance Plan</span>
+                    <div class="left-form-section">
+                        <div class="form-card px-0 py-4 mt-2">
+                            <form>
+                                <div class="row">
+
+                                    <div class="col-sm-5 border-right">
+                                        <div class="form-row">
+                                            <div class="form-group col-12">
+                                                <label class="vag-r-bold mb-0 text-primary font-17" for="inputName">Name</label>
+                                                <input type="text" class="form-control" id="inputName">
+                                            </div>
+                                            <div class="form-group col-12">
+                                                <label class="vag-r-bold mb-0 text-primary font-17" for="inputGender">Gender</label>
+                                                <select id="inputGender" class="form-control">
+                                                    <option selected>Please Select</option>
+                                                    <option>Male</option>
+                                                    <option>Female</option>
+                                                    <option>Other</option>
+                                                </select>
+                                            </div>
+                                            <div class="form-group col-12">
+                                                <label class="vag-r-bold mb-0 text-primary font-17" for="inputEmail">Email</label>
+                                                <input type="email" class="form-control" id="inputEmail">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-5">
+                                        <div class="form-row">
+                                            <div class="form-group col-12">
+                                                <label class="vag-r-bold mb-0 text-primary font-17" for="inputMobile">Mobile Number</label>
+                                                <input type="text" class="form-control" id="inputMobile">
+                                            </div>
+                                            <div class="form-group col-12">
+                                                <label class="vag-r-bold mb-0 text-primary font-17" for="inputState">State</label>
+                                                <select id="inputState" class="form-control">
+                                                    <option selected>Please Select</option>
+                                                    <option>Maharashtra</option>
+                                                </select>
+                                            </div>
+                                            <div class="form-group col-12">
+                                                <label class="vag-r-bold mb-0 text-primary font-17" for="inputCity">City</label>
+                                                <input type="text" class="form-control" id="inputCity">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-10">
+                                        <div class="form-row">
+                                            <div class="form-group mt-2">
+                                                <div class="custom-control custom-checkbox">
+                                                    <input type="checkbox" class="custom-control-input" id="chkContactDetails">
+                                                    <label class="custom-control-label line-height-1 font-9" for="chkContactDetails">By submitting my contact details here, I override my NDNC registration and authorize Reliance Nippon Life Insurance Company and its representative to contact me through sms/ call/ e-mail.</label>
+                                                </div>
+                                            </div>
+                                            <div class="form-group mb-0">
+                                                <button type="submit" class="btn btn-primary vag-r-bold letter-spacing-05 font-16 px-3 py-0 font-16 font-weight-bold">Submit</button>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-5 col-xl-7 right-section align-self-center">
+                <div class="text-center mt-4">
+                    <img src="./public/images/png/home-banner.png" class="img-fluid" alt="Protect" width="" title="Protect" />
+                </div>
+            </div>
+        </div>
+        <div class="row mx-0">
+            <div class="col-12">
+                <img src="./public/images/png/home-banner-bottom.png" class="img-fluid px-2" alt="Steps">
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+<!--********************** Variant Section ***********************-->
+<section class="bg-white">
+    <div class="container-fluid">
+        <div class="row">
+            <!--****** Variant 1 ******-->
+            <div class="col-12 px-0">
+                <div>
+                    <img src="./public/images/png/variant/variant-1.png" class="img-fluid" alt="Variant 1">
+                </div>
+            </div>
+
+            <!--****** Variant 2 ******-->
+            <div class="col-12 px-0">
+                <div>
+                    <img src="./public/images/png/variant/variant-2.png" class="img-fluid" alt="Variant 2">
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
+
+
+<!--********************** Why Reliance Insurance Section ***********************-->
+<section>
+    <div class="container-fluid px-0">
+        <div class="row">
+            <div class="col-12">
+                <img src="./public/images/png/why-reliance-insurance.png" class="img-fluid" alt="Why Reliance Insurance?">
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<?php include 'footer.php' ?>
